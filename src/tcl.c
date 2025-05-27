devinkbrown/eggdrop

commit 53dcf4d0bd3ce377e246c2c8ceac6d8352d52b16

RPL_ISUPPORT parsing, Tcl API and using trivial values (#438)
Patch by: thommey
Closes: #3 

Basic RPL_ISUPPORT (IRC raw 005) parsing and Tcl API. Allowed to expand and use the values later, but that is out of the scope of this.

- Adds Tcl commands: isupport get, isupport isset, and the ISUPPORT bind.
- If a setting is not defined in the 005 message, the following default values are used:
"CASEMAPPING=rfc1459 CHANNELLEN=80 NICKLEN=9 CHANTYPES=#& PREFIX=(ov)@+ CHANMODES=b,k,l,imnpst MODES=3 MAXCHANNELS=10 TOPICLEN=250 KICKLEN=250 STATUSMSG=@+"
- Provided network names are displayed in .status
- WHOX support is detected and set appropriately for Eggdrop to use
- Set internal values for MODES, MAXLIST,  MAXEXCEPT, MAXBAN, NICKLEN/MAXNICKLEN based on parsed values
- Probably other stuff, too

src/tcl.c
@@ -297,6 +297,41 @@ static char *tcl_eggstr(ClientData cdata, Tcl_Interp *irp,
   }
 }
 
+struct tcl_call_stringinfo {
+  Tcl_CmdProc *proc;
+  ClientData cd;
+};
+
+static void tcl_cleanup_stringinfo(ClientData cd)
+{
+  nfree(cd);
+}
+
+/* Compatibility wrapper that calls Tcl functions with String API */
+static int tcl_call_stringproc_cd(ClientData cd, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
+{
+  static int max;
+  static CONST84 char **argv;
+  int i;
+  struct tcl_call_stringinfo *info = cd;
+  /* The string API guarantees argv[argc] == NULL, unlike the obj API */
+  if (objc + 1 > max)
+    argv = nrealloc(argv, (objc + 1) * sizeof *argv);
+  for (i = 0; i < objc; i++)
+    argv[i] = Tcl_GetString(objv[i]);
+  argv[objc] = NULL;
+  return (info->proc)(info->cd, interp, objc, argv);
+}
+
+/* The standard case of no actual cd */
+static int tcl_call_stringproc(ClientData cd, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
+{
+  struct tcl_call_stringinfo info;
+  info.proc = cd;
+  info.cd = NULL;
+  return tcl_call_stringproc_cd(&info, interp, objc, objv);
+}
+
 /* Add/remove tcl commands
  */
 
@@ -305,14 +340,21 @@ void add_tcl_commands(tcl_cmds *table)
   int i;
 
   for (i = 0; table[i].name; i++)
-    Tcl_CreateCommand(interp, table[i].name, table[i].func, NULL, NULL);
+    Tcl_CreateObjCommand(interp, table[i].name, tcl_call_stringproc, table[i].func, NULL);
 }
 
 void add_cd_tcl_cmds(cd_tcl_cmd *table)
 {
+  struct tcl_call_stringinfo *info;
   while (table->name) {
-    Tcl_CreateCommand(interp, table->name, table->callback,
-                      (ClientData) table->cdata, NULL);
+    if (table->cdata) {
+      info = nmalloc(sizeof *info);
+      info->proc = table->callback;
+      info->cd = table->cdata;
+      Tcl_CreateObjCommand(interp, table->name, tcl_call_stringproc_cd, info, tcl_cleanup_stringinfo);
+    } else {
+      Tcl_CreateObjCommand(interp, table->name, tcl_call_stringproc, table->callback, NULL);
+    }
     table++;
   }
 }

.gitignore
@@ -32,7 +32,7 @@ Makefile
 .known_modules
 /config.h
 /eggint.h
-lush.h
+/lush.h
 config.log
 config.status
 config.cache

eggdrop.conf
@@ -1214,6 +1214,11 @@ set optimize-kicks 1
 # command, or set this to 0 for unlimited.
 set stack-limit 4
 
+# Sets the default RPL_ISUPPORT (raw 005) information to use as a fallback.
+# These MUST be compatible to all IRCds you might want to connect to.
+# The actual server settings overwrite these, so you shouldn't need to modify.
+#set isupport-default "CASEMAPPING=rfc1459 CHANNELLEN=80 NICKLEN=9 CHANTYPES=#& PREFIX=(ov)@+ CHANMODES=b,k,l,imnpst MODES=3 MAXCHANNELS=10 TOPICLEN=250 KICKLEN=250 STATUSMSG=@+"
+
 ### SERVER MODULE - OTHER NETWORKS (net-type "Other") ###
 
 # This settings defines how umode +r is understood by Eggdrop. Some networks
@@ -1269,24 +1274,6 @@ set bounce-invites 0
 
 # Set this to 1 if you want to bounce all server modes.
 set bounce-modes 0
-
-# Set here the maximum number of bans you want the bot to set on a channel.
-# Eggdrop will not place any more bans if this limit is reached. Undernet
-# currently allows 45 bans, IRCnet allows 64, EFnet allows 100, and DALnet
-# allows 100.
-set max-bans 30
-
-# Set here the maximum number of exempts you want Eggdrop to set on a channel.
-# Eggdrop will not place any more exempts if this limit is reached.
-set max-exempts 20
-
-# Set here the maximum number of invites you want Eggdrop to set on a channel.
-# Eggdrop will not place any more invites if this limit is reached.
-set max-invites 20
-
-# There is a global limit for +b/+e/+I modes. This limit should be set to
-# the same value as max-bans for networks that do not support +e/+I.
-set max-modes 30
 
 # The following settings should be left commented unless the default values
 # are being overridden. By default, exempts and invites are on for EFnet and
@@ -1364,22 +1351,10 @@ set prevent-mixing 1
 # once, or set this to 0 for all at once.
 #set kick-method 1
 
-# Some networks allow you to stack lots of channel modes into one line.
-# They're all guaranteed to support at least 3, so that's the default.
-# If you know your network supports more, you may want to adjust this.
-# This setting is limited to 6, although if you want to use a higher value,
-# you can modify this by changing the value of MODES_PER_LINE_MAX in
-# src/chan.h and recompiling the bot.
-#set modes-per-line 3
-
 # Some networks don't include the +l limit and +k or -k key modes
-# in the modes-per-line (see above) limitation. Set include-lk to 0 for
+# in the modes-per-line limitation. Set include-lk to 0 for
 # these networks.
 #set include-lk 1
-
-# Set this to 1 if your network uses IRCu2.10.01 specific /who requests.
-# Eggdrop can, therefore, ask only for exactly what's needed.
-#set use-354 0
 
 # If your network doesn't use rfc 1459 compliant string matching routines,
 # set this to 0.

src/main.h
@@ -144,4 +144,6 @@ extern struct dcc_table DCC_CHAT, DCC_BOT, DCC_LOST, DCC_SCRIPT, DCC_BOT_NEW,
 
 #define EGG_AC_ARGS EGG_MACRO_STR(EGG_AC_ARGS_RAW)
 
+#define ARRAY_SIZE(x) (sizeof (x) / sizeof *(x))
+
 #endif /* _EGG_MAIN_H */

src/mod/irc.mod/chan.c
@@ -2552,6 +2552,92 @@ static int gotnotice(char *from, char *msg)
   return 0;
 }
 
+static int parse_maxlist(const char *value)
+{
+  int tmpsum = 0, addtosum;
+  char *tmps;
+  long tmpl;
+  const char *modechar = value, *number;
+
+  max_exempts = 20;
+  max_invites = 20;
+  max_bans = 30;
+  max_modes = 30;
+
+  if (!value || *value == ':') {
+    return 0;
+  }
+  /* e.g. value="be:100,I:50" */
+  do {
+    number = strchr(modechar, ':');
+    if (!number) {
+      putlog(LOG_MISC, "*", "Error while parsing ISUPPORT value for MAXLIST: number not found in '%s'", value);
+      return -1;
+    }
+    number++;
+    tmpl = strtol(number, &tmps, 10);
+    if (*tmps != '\0' && *tmps != ',') {
+      putlog(LOG_MISC, "*", "Error while parsing ISUPPORT value for MAXLIST: invalid number in '%s'", value);
+      return -2;
+    }
+    if (tmpl < 10) {
+      putlog(LOG_MISC, "*", "Warning while parsing ISUPPORT value for MAXLIST: number too small, setting to 10 in '%s'", value);
+      tmpl = 10;
+    } else if (tmpl > 100000) {
+      putlog(LOG_MISC, "*", "Warning while parsing ISUPPORT value for MAXLIST: number too big, setting to 100000 in '%s'", value);
+      tmpl = 100000;
+    }
+    addtosum = 0;
+    do {
+      if (*modechar == 'b') {
+        max_bans = (int)tmpl;
+        addtosum = 1;
+      } else if (*modechar == 'e') {
+        max_exempts = (int)tmpl;
+        addtosum = 1;
+      } else if (*modechar == 'I') {
+        max_invites = (int)tmpl;
+        addtosum = 1;
+      } else {
+        continue;
+      }
+    } while (*modechar++ != ':');
+    /* Build maxmodes from all sections that interest us for now (beI),
+     * e.g. be:100,I:100 means maxmodes=200 (limit for bans + excepts + invites) */
+    if (addtosum) {
+      tmpsum += (int)tmpl;
+    }
+    modechar = tmps;
+  } while (*modechar++ == ',');
+
+  max_modes = tmpsum;
+  return 0;
+}
+
+static int irc_isupport(char *key, char *isset_str, char *value)
+{
+  int isset = !strcmp(isset_str, "1");
+
+  if (!strcmp(key, "WHOX")) {
+    use_354 = isset;
+  } else if (!strcmp(key, "MODES")) {
+    isupport_parseint(key, isset ? value : NULL, 3, 64, 1, 3, &modesperline);
+  } else if (!strcmp(key, "MAXLIST")) {
+    parse_maxlist(isset ? value : NULL);
+  } else if (!strcmp(key, "MAXEXCEPTS")) {
+    isupport_parseint(key, isset ? value : NULL, 10, 100000, 1, 20, &max_exempts);
+    if (max_exempts > max_modes) {
+      max_modes = max_exempts;
+    }
+  } else if (!strcmp(key, "MAXBANS")) {
+    isupport_parseint(key, isset ? value : NULL, 10, 100000, 1, 30, &max_bans);
+    if (max_bans > max_modes) {
+      max_modes = max_bans;
+    }
+  }
+  return 0;
+}
+
 static cmd_t irc_raw[] = {
   {"324",     "",   (IntFunc) got324,          "irc:324"},
   {"352",     "",   (IntFunc) got352,          "irc:352"},
@@ -2583,6 +2669,11 @@ static cmd_t irc_raw[] = {
   {"348",     "",   (IntFunc) got348,          "irc:348"},
   {"349",     "",   (IntFunc) got349,          "irc:349"},
   {NULL,      NULL, NULL,                           NULL}
+};
+
+static cmd_t irc_isupport_binds[] = {
+  {"*",       "",   (IntFunc) irc_isupport, "irc:isupport"},
+  {NULL,    NULL,   NULL,                             NULL}
 };
 
 static cmd_t irc_rawt[] = {

src/mod/irc.mod/irc.c
@@ -38,10 +38,10 @@ static Function *global = NULL, *channels_funcs = NULL, *server_funcs = NULL;
 
 static int ctcp_mode;
 static int wait_split = 300;    /* Time to wait for user to return from net-split. */
-static int max_bans = 20;       /* Modified by net-type 1-4 */
+static int max_bans = 30;       /* Modified by net-type 1-4 */
 static int max_exempts = 20;    /* Modified by net-type 1-4 */
 static int max_invites = 20;    /* Modified by net-type 1-4 */
-static int max_modes = 20;      /* Modified by net-type 1-4 */
+static int max_modes = 30;      /* Modified by net-type 1-4 */
 static int bounce_bans = 0;
 static int bounce_exempts = 0;
 static int bounce_invites = 0;
@@ -1228,6 +1228,7 @@ static char *irc_close()
   rem_builtins(H_msg, C_msg);
   rem_builtins(H_raw, irc_raw);
   rem_builtins(H_rawt, irc_rawt);
+  rem_builtins(H_isupport, irc_isupport_binds);
   rem_tcl_commands(tclchan_cmds);
   rem_help_reference("irc.help");
   del_hook(HOOK_MINUTELY, (Function) check_expired_chanstuff);
@@ -1332,6 +1333,7 @@ char *irc_start(Function *global_funcs)
   add_builtins(H_msg, C_msg);
   add_builtins(H_raw, irc_raw);
   add_builtins(H_rawt, irc_rawt);
+  add_builtins(H_isupport, irc_isupport_binds);
   add_tcl_commands(tclchan_cmds);
   add_help_reference("irc.help");
   H_topc = add_bind_table("topc", HT_STACKABLE, channels_5char);

src/mod/module.h
@@ -508,7 +508,7 @@
 #define check_validpass ((char *(*) (struct userrec *, char *))global[307])
 /* 308 - 311 */
 #define make_rand_str_from_chars ((void (*) (char *, int, char *))global[308])
-
+#define add_tcl_objcommands ((void (*) (tcl_cmds *))global[309])
 
 /* hostmasking */
 #define maskhost(a,b) maskaddr((a),(b),3)

src/mod/server.mod/Makefile
@@ -38,7 +38,8 @@ distclean: clean
  ../../../src/compat/gethostbyname2.h \
  ../../../src/compat/explicit_bzero.h ../../../src/compat/strlcpy.h \
  ../../../src/mod/modvals.h ../../../src/tandem.h \
- .././server.mod/server.h .././server.mod/servmsg.c \
+ .././server.mod/server.h .././server.mod/isupport.c \
+ .././server.mod/tclisupport.c .././server.mod/servmsg.c \
  .././server.mod/../irc.mod/irc.h \
  .././server.mod/../channels.mod/channels.h .././server.mod/cmdsserv.c \
  .././server.mod/tclserv.c

commit b60766a4d66dec2e1d01c3a9c631c55c088cb683

Fix use after free in tcl_call_stringproc_cd
Found by: @kkwpsi
Patch by: @michaelortmann

One-line summary:
Fix use after free in tcl_call_stringproc_cd() - be reentrant, do not nrealloc

Broken since #438

src/tcl.c
@@ -316,20 +316,31 @@ static void tcl_cleanup_stringinfo(ClientData cd)
   nfree(cd);
 }
 
-/* Compatibility wrapper that calls Tcl functions with String API */
+/* Compatibility wrapper that calls Tcl functions with String API
+ *
+ * Is reentrant, can call itself recursively, so argv is dynamically allocated
+ * Tcl_IncrRefCount() is needed to preserve the strings we get Tcl_GetString()
+ * from being cleaned up if Tcl is invoked from this
+ */
 static int tcl_call_stringproc_cd(ClientData cd, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
 {
-  static int max;
-  static const char **argv;
-  int i;
+  const char **argv;
+  int i, ret;
   struct tcl_call_stringinfo *info = cd;
+
   /* The string API guarantees argv[argc] == NULL, unlike the obj API */
-  if (objc + 1 > max)
-    argv = nrealloc(argv, (objc + 1) * sizeof *argv);
-  for (i = 0; i < objc; i++)
+  argv = nmalloc((objc + 1) * sizeof *argv);
+  for (i = 0; i < objc; i++) {
+    Tcl_IncrRefCount(objv[i]);
     argv[i] = Tcl_GetString(objv[i]);
+  }
   argv[objc] = NULL;
-  return (info->proc)(info->cd, interp, objc, argv);
+  ret = (info->proc)(info->cd, interp, objc, argv);
+  for (i = 0; i < objc; i++) {
+    Tcl_DecrRefCount(objv[i]);
+  }
+  nfree(argv);
+  return ret;
 }
 
 /* The standard case of no actual cd */