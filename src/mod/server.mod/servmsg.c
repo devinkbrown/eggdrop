devinkbrown/eggdrop

commit d27343f706718f16bb6a46b148b87cca43fa2d9f

Limit buffer to 512 bytes
Found by: michaelortmann
Patch by: michaelortmann

src/mod/server.mod/servmsg.c
@@ -691,7 +691,8 @@ static int gotmsg(char *from, char *msg)
  */
 static int gotnotice(char *from, char *msg)
 {
-  char *to, *nick, ctcpbuf[512], *p, *p1, buf[512], *uhost = buf, *ctcp;
+  #define CTCP_MAX 512
+  char *to, *nick, ctcpbuf[CTCP_MAX], *p, *p1, buf[512], *uhost = buf, *ctcp;
   struct userrec *u;
   int ignoring;
 
@@ -714,7 +715,13 @@ static int gotnotice(char *from, char *msg)
       p++;
     if (*p == 1) {
       *p = 0;
-      ctcp = strcpy(ctcpbuf, p1);
+      if ((p - p1) < sizeof ctcpbuf) {
+        putlog(LOG_SERV, "*", "Warning: Got NOTICE CTCP reply longer than "
+               STRINGIFY(CTCP_MAX) " bytes: Bogus server?");
+        return 0;
+      }
+      strlcpy(ctcpbuf, p1, sizeof ctcpbuf);
+      ctcp = ctcpbuf;
       memmove(p1 - 1, p + 1, strlen(p + 1) + 1);
       if (!ignoring)
         detect_flood(nick, uhost, from, FLOOD_CTCP);

src/net.c
@@ -1377,7 +1377,7 @@ void tputs(int z, char *s, unsigned int len)
     inhere = 1;
 
     putlog(LOG_MISC, "*", "!!! writing to nonexistent socket: %d", z);
-    s[strlen(s) - 1] = 0;
+    s[len - 1] = 0;
     putlog(LOG_MISC, "*", "!-> '%s'", s);
 
     inhere = 0;

commit 3a74f306b2ccc3dbc9d5c8639b401769f24617a1

Fix gotnotice bad termination
Found by: skydrome
Patch by: @michaelmortmann

Broken in #1691

src/mod/server.mod/servmsg.c
@@ -715,7 +715,7 @@ static int gotnotice(char *from, char *msg)
       p++;
     if (*p == 1) {
       *p = 0;
-      if ((p - p1) < sizeof ctcpbuf) {
+      if ((p - p1) >= sizeof ctcpbuf) {
         putlog(LOG_SERV, "*", "Warning: Got NOTICE CTCP reply longer than "
                STRINGIFY(CTCP_MAX) " bytes: Bogus server?");
         return 0;